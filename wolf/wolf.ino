396ae74c198fb7adc47830d71d8e169bafc22127
Jozsef Martin <jozsef.martin@gmail.com>
2026-06-06 19:22:48 +0200

    Refactor and enhance melody generation and control logic

--- wolf/wolf.ino
+++ wolf/wolf.ino
@@ -1,6 +1,23 @@
  /*
-  * Wolf. Generative Synthesizer
+  * Wolf. 
+  * A Generative Synthesizer
+  * 
   * 4 Potentiometers (A0, A1, A2, A4) + 1 Button (D3)
+  * 
+  * A0-Normal: Mix ( Bass <==>Pad )
+  * A0-Button: Change scale
+  * 
+  * A1-Normal: Note change speed (200 - 4)
+  * A1-Button: Shift melody )
+  * 
+  * A2-Normal: Pad note length (5 - 250 ms)
+  * A2-Button: Bass note length (5 - 250 ms) * 2
+  * 
+  * A4-Normal: LPF cutoff (10 - 245)
+  * A4-Button: Mutation speed
+  * 
+  * Button double press: generate new melody + change scale
+  *  
   */
 
 #include <MozziGuts.h>
@@ -8,16 +25,24 @@
 #include <LowPassFilter.h>
 #include <tables/sin2048_int8.h>
 #include <tables/saw8192_int8.h>
+//#include <tables/sin8192_int8.h>
 #include <tables/smoothsquare8192_int8.h> 
 #include <mozzi_midi.h>
-#include <EventDelay.h> 
+//#include <EventDelay.h> 
 
-#define CONTROL_RATE 128
+#define CONTROL_RATE 64
+
+#define MAX_NOTE_LENGTH CONTROL_RATE * 16 // 1024 ms at CONTROL_RATE of 64
+#define MAX_STEP_LENGTH CONTROL_RATE * 16 // 1024 ms at CONTROL_RATE of 64
+
 #define BUTTON_PIN 3                     
 #define PRESSED LOW
-#define minAnalogChange 10
+#define minAnalogChange 3
+#define REST 99
+#define MELODY_LENGTH 16
 
 Oscil<SAW8192_NUM_CELLS, AUDIO_RATE> aPad(SAW8192_DATA); 
+//Oscil<SIN8192_NUM_CELLS, AUDIO_RATE> aBass(SIN8192_DATA);
 Oscil<SMOOTHSQUARE8192_NUM_CELLS, AUDIO_RATE> aBass(SMOOTHSQUARE8192_DATA);
 LowPassFilter lpf;
 
@@ -26,17 +51,24 @@ Oscil<SIN2048_NUM_CELLS, CONTROL_RATE> lfoPad(SIN2048_DATA);
 
 byte currentScale = 0; 
 const byte scaleSize = 14;
-const byte numScales = 5;
+const byte numScales = 6;
 
 const byte scales[numScales][scaleSize] = {
-  {33, 35, 36, 38, 40, 42, 43, 45, 47, 48, 50, 52, 54, 55}, // 0: A-Dorian
-  {33, 34, 36, 38, 40, 41, 43, 45, 46, 48, 50, 51, 53, 54}, // 1: A-Phrygian
-  {36, 38, 40, 42, 43, 45, 47, 50, 52, 54, 55, 57, 59, 62}, // 2: C-Lydian (felülről nyitott)
-  {33, 35, 36, 38, 40, 42, 43, 45, 47, 48, 50, 52, 54, 55}, // 0: A-Dorian
-  {33, 34, 36, 38, 40, 41, 43, 45, 46, 48, 50, 51, 53, 54}  // 1: A-Phrygian
+    {33, 35, 36, 38, 40, 42, 43, 45, 47, 48, 50, 52, 54, 55}, // G-dur / A-dor
+    {33, 34, 36, 38, 40, 41, 43, 45, 46, 48, 50, 51, 53, 54}, // Szintetikus
+    {36, 38, 40, 42, 43, 45, 47, 50, 52, 54, 55, 57, 59, 62}, // C-lid
+    {33, 35, 36, 38, 40, 41, 43, 45, 47, 48, 50, 51, 53, 55}, // A-moll
+    {33, 36, 38, 40, 43, 45, 48, 50, 52, 55, 57, 60, 62, 64}, // A-pentaton
+    {33, 36, 38, 39, 40, 43, 45, 48, 50, 51, 52, 55, 57, 60}  // A-blues
 };
 
-byte melody[16];
+// Step probability array for melody generation and mutation
+byte stepsSize = 9;
+const int steps[] = {-2, 2, -1, -1, 1, 1, 0, REST, REST};
+int step;
+
+// Melody array includes note indexes and not the notes! 
+byte melody[MELODY_LENGTH];
 
 int oldA0 = 0, oldA1 = 0, oldA2 = 0, oldA4 = 0;
 int mixPot, lfoPot, bpmPo, lpfPot;
@@ -47,18 +79,22 @@ bool lastButtonState = HIGH;
 unsigned long lastButtonPressedTime = 0;
 unsigned long shortPressThreshold = 800000; // 200 ms
 
-unsigned int timeCounter = 0;
-unsigned int changeThreshold = 300; 
+unsigned int ticks = 0;
+unsigned int stepLength = 300; 
 byte noteIndex = 0;
 
-long bassGain = 0;
-long padGain = 0;
+long bassGain = 200;
+long padGain = 200;
+int bassNotelength = 200;
+int padNotelength = 200;
+
+byte mutationSpeed = 20; // in ticks
 
 void setup() {
   pinMode(BUTTON_PIN, INPUT_PULLUP);     
   pinMode(LED_BUILTIN, OUTPUT);          
   
-  randomSeed(analogRead(A4));            
+  randomSeed(analogRead(A5));            
   startMozzi(CONTROL_RATE);
   
   lfoBass.setFreq(0.25f);    
@@ -72,19 +108,49 @@ void setup() {
 }
 
 void generateNewMelody() {
-  for (int i = 0; i < 16; i++) {
-    if (random(0, 100) < 30) { 
-      melody[i] = 0; 
+  byte idx = 0; //random(0, scaleSize);
+  for (int i = 0; i < MELODY_LENGTH; i++) {
+    step = steps[random(0, stepsSize)];
+    if (step == REST) {
+      melody[i] = REST; 
     } else {
-      melody[i] = scales[currentScale][random(0, scaleSize)];
+      idx = (idx + step);
+      if (idx < 0) {
+        idx = 0;
+      }
+      if (idx >= scaleSize){
+        idx = scaleSize-1;
+      }
+      melody[i] = idx;
+    }
+  }
+}
+
+void mutateMelody() {
+  byte idx = random(0, MELODY_LENGTH);
+  step = steps[random(0, stepsSize)];
+  if (step == REST) {
+    melody[idx] = REST; 
+  } else {
+    if (melody[idx] = REST) { 
+      melody[idx] = random(0, scaleSize); // If it's currently a pause, change it to a random note
+    } else {
+      int newIdx = melody[idx] + step;
+      if (newIdx < 0) {
+        newIdx = 0;    
+      }
+      if (newIdx >= scaleSize){
+        newIdx = scaleSize-1;
+      }
+      melody[idx] = newIdx;
     }
   }
 }
 
 void nextNote(){
-  if (melody[noteIndex] != 0) {
-    aPad.setFreq(mtof(melody[noteIndex]));
-    aBass.setFreq(mtof(melody[noteIndex] - 12)); 
+  if (melody[noteIndex] != REST) {
+    aPad.setFreq(mtof(scales[currentScale][melody[noteIndex]]));
+    aBass.setFreq(mtof(scales[currentScale][melody[noteIndex]])); 
   }
   noteIndex = (noteIndex + 1) % 16;
 }
@@ -107,12 +173,12 @@ void updateControl() {
     if (mozziMicros() - lastButtonPressedTime < shortPressThreshold) { 
       currentScale = (currentScale + 1) % numScales;
       generateNewMelody(); 
-      digitalWrite(LED_BUILTIN, HIGH);
     }
     lastButtonPressedTime = mozziMicros();
   }
-  if (buttonState == !PRESSED) { digitalWrite(LED_BUILTIN, LOW); }
-  
+  digitalWrite(LED_BUILTIN, buttonState==PRESSED);
+
+
   // handle A0 with or without button press - only if A0 was moved! (using oldA0)
   int newA0 = mozziAnalogRead(A0);
   if (abs(newA0 - oldA0) > minAnalogChange) {  // Only update if the value has changed significantly
@@ -129,22 +195,22 @@ void updateControl() {
   if (abs(newA1 - oldA1) > minAnalogChange) {  // Only update if the value has changed significantly
     oldA1 = newA1;  // Update oldA1 to the new value 
     if (buttonState == PRESSED) {
-      changeThreshold = map(newA1, 0, 1023, 600, 4) * 2; 
+      stepLength = map(newA1, 0, 1023, 200, 4) * 2; 
     } else {
-      changeThreshold = map(newA1, 0, 1023, 600, 4); 
+      stepLength = map(newA1, 0, 1023, 200, 4); 
     }
   }
 
- // handle A2 with or without button press - only if A2 was moved! (using oldA2)
+  // handle A2 with or without button press - only if A2 was moved! (using oldA2)
   int newA2 = mozziAnalogRead(A2);
   if (abs(newA2 - oldA2) > minAnalogChange) {  // Only update if the value has changed significantly
     oldA2 = newA2;  // Update oldA2 to the new value 
     if (buttonState == PRESSED) {
-      lfoBass.setFreq(map(newA2, 0, 1023, 1, 400) / 100.0f * 2); 
-      lfoPad.setFreq(map(newA2, 0, 1023, 1, 400) / 100.0f * 1.2f); 
+      bassNotelength = map(newA2, 0, 1023, 5, 250);
+      padNotelength = map(newA2, 0, 1023, 5, 250); 
     } else {
-      lfoBass.setFreq(map(newA2, 0, 1023, 1, 400) / 100.0f); 
-      lfoPad.setFreq(map(newA2, 0, 1023, 1, 400) / 100.0f * 0.6f); 
+      bassNotelength = map(newA2, 0, 1023, 5, 250);
+      padNotelength = map(newA2, 0, 1023, 5, 250); 
     }
   }
 
@@ -153,7 +219,7 @@ void updateControl() {
   if (abs(newA4 - oldA4) > minAnalogChange) {  // Only update if the value has changed significantly
     oldA4 = newA4;  // Update oldA4 to the new value 
     if (buttonState ==  PRESSED) {
-      lpf.setCutoffFreq(map(newA4, 0, 1023, 10, 245) * 2); 
+      mutationSpeed = map(newA4, 0, 1023, 0, 255); // in ticks
     } else {
       lpf.setCutoffFreq(map(newA4, 0, 1023, 10, 245)); 
     }
@@ -161,21 +227,32 @@ void updateControl() {
   
  
   // LFO modulation
-  long bassVolumeMod = map(lfoBass.next(), -127, 128, 40, 255);
-  long padVolumeMod = map(lfoPad.next(), -127, 128, 20, 255);
+  // long bassVolumeMod = map(lfoBass.next(), -127, 128, 40, 255);
+  // long padVolumeMod = map(lfoPad.next(), -127, 128, 20, 255);
   
   // Note change timing
-  timeCounter++;
-  if (timeCounter >= changeThreshold) {
-    timeCounter = 0;
+  ticks++;
+  if (ticks >= stepLength) {
+    ticks = 0;
+    if (random(5, 250) < mutationSpeed) {
+      mutateMelody();
+    }
     nextNote();
   }
+
+  bassGain = (1023 - mixPot) >> 2;
+  padGain = mixPot >> 2;
   
-  long bassRatio = 1023 - mixPot;
-  long padRatio = mixPot;
-  
-  bassGain = (bassVolumeMod * bassRatio) >> 10;
-  padGain = (padVolumeMod * padRatio) >> 10;
+  if (ticks > bassNotelength) {
+    bassGain = 0; 
+  }
+  if (ticks > padNotelength) {
+    padGain = 0; 
+  }
+  if (melody[noteIndex] == REST) {
+    //bassGain = 0; 
+    padGain = 0; 
+  }
 }
 
 int updateAudio() {
